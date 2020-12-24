270d672c6001d05fd9f4ebdacc653a970c39d925
pngnwthbrd <coding@pngnwthbrd.com>
2020-12-25 00:09:40 +0100

    basis test with config

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,11 +1,12 @@
 import sys
 import os
+from pysteamapi.config import cfg
 
 sys.path.append(os.path.abspath(
                 os.path.join(os.path.dirname(__file__), '..')))
-from pysteamapi import steam
+from steamwishlistscomparison import steam
 
-steam = steam.Steam(steamid = PUT_IN_STEAM_ID)
+steam = steam.Steam(steamid = cfg['steam']['steamid'])
 
 print(steam.get_friendslist())
 #print(steam._get_steamid_by_vanityurl())
